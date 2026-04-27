c79962861c21e950401b2eca69d78e1ca581b9fc
trang112 <86561813+trang112@users.noreply.github.com>
2026-04-27 15:58:50 +0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,4 +1,12 @@
-
+/*
+=====================================================
+DDL Script: create silver tables
+=====================================================
+Script purpose: 
+  This script creates tables in the 'silver' schema, dropping existing tables if they already exist
+  Run this script to re define the DDL structure of 'silver' tables
+=====================================================
+*/
 -- Create silver layers
 USE Datawarehouse
 GO
